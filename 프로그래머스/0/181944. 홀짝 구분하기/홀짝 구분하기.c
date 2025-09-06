ff0c79318b517af02ce27f8a2dc946e7640964eb
twg041110 <150268649+twg041110@users.noreply.github.com>
2025-09-06 23:22:38 +0900

    [level 0] Title: 홀짝 구분하기, Time: 2.60 ms, Memory: 4.28 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    
+    if(n % 2 == 0){
+        printf("%d is even", n);
+    }
+    else{
+        printf("%d is odd", n);
+    }
+        
+    return 0;
+}
